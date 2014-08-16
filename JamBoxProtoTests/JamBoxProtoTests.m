ab08d9b6d595563106b05c9fabf9e3265b8621a9
Maxim Lakin <maximlakin@Maxims-MacBook-Air.local>
2014-08-16 04:46:54 -0400

    Initial Commit

---
+++ JamBoxProtoTests/JamBoxProtoTests.m
@@ -0,0 +1,34 @@
+//
+//  JamBoxProtoTests.m
+//  JamBoxProtoTests
+//
+//  Created by Maxim Lakin on 8/16/14.
+//  Copyright (c) 2014 Maxim Lakin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JamBoxProtoTests : XCTestCase
+
+@end
+
+@implementation JamBoxProtoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
